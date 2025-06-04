5cbb429df46a01023db326a61d1fbb9bce5276fa
Matt Pryor <matt.pryor@nscale.com>
2025-06-04 17:29:40 +0100

    Add types for label selectors

--- easykube/__init__.py
+++ easykube/__init__.py
@@ -1,10 +1,1 @@
-from .kubernetes import (
-    ABSENT,  # noqa: F401
-    PRESENT,  # noqa: F401
-    ApiError,  # noqa: F401
-    AsyncClient,  # noqa: F401
-    Configuration,  # noqa: F401
-    ResourceSpec,  # noqa: F401
-    SyncClient,  # noqa: F401
-    resources,  # noqa: F401
-)
+from .kubernetes import *  # noqa: F403
--- easykube/kubernetes/client/__init__.py
+++ easykube/kubernetes/client/__init__.py
@@ -2,5 +2,12 @@ from .api import Api  # noqa: F401
 from .client import AsyncClient, SyncClient  # noqa: F401
 from .errors import ApiError  # noqa: F401
 from .iterators import ListResponseIterator, WatchEvents  # noqa: F401
-from .resource import ABSENT, PRESENT, DeletePropagationPolicy, Resource  # noqa: F401
+from .resource import (  # noqa: F401
+    ABSENT,
+    PRESENT,
+    DeletePropagationPolicy,
+    LabelSelectorSpecial,
+    LabelSelectorValue,
+    Resource,
+)
 from .spec import ResourceSpec  # noqa: F401
